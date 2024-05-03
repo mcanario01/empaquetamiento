8711ecd29480473be8f7cbc8f6b536d7584613db
JoaquinArgel <168217660+JoaquinArgel@users.noreply.github.com>
2024-05-03 00:19:21 -0400

    Update funciones.cpp

--- Tarea1_Protocolo/funciones.cpp
+++ Tarea1_Protocolo/funciones.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "funciones.h"
+#include <string.h>
 
 
 int empaquetar(Protocolo & proto)
@@ -10,7 +11,6 @@ int empaquetar(Protocolo & proto)
 
     proto.Frames[0] = proto.CMD & 0x7F | proto.LNG & 0x01 << 7; // guardar CMD(7) y LNG(1), no sobra nada
     proto.Frames[1] = (proto.LNG >> 1) & 0x1F; // guarda los siguientes 5 bits, faltan 3
-
     if(proto.LNG > 0) // si quedan datos por enviar, se encapsulan
     {
         for(size_t i = 0; i < proto.LNG; i++)
@@ -18,34 +18,36 @@ int empaquetar(Protocolo & proto)
             proto.Frames[i + 2] = proto.DATA[i];
         }
     }
-    proto.FCS[0] = fcs(proto.Frames, proto.LNG + 2); //PROBLEMAS
-    proto.Frames[proto.LNG+2] = proto.FCS & 0xFF; //calcula y guarda fsc de Frames en frames
-    proto.Frames[proto.LNG + 3] = (proto.FCS >> 8) & 0x03;
+    unsigned int fcsValue = fcs(proto.Frames, proto.LNG+2);
+    proto.FCS[0] = (fcsValue >> 8) & 0xFF;  
+    proto.FCS[1] = fcsValue & 0xFF;  
+    proto.Frames[proto.LNG+2] = proto.FCS[0]; //calcula y guarda fsc de Frames en frames
+    proto.Frames[proto.LNG + 3] = proto.FCS[1];
     return proto.LNG + 4;
 }
 
-int fcs(BYTE * arr, int size)
-{
-    int suma_bits = 0;
-    for(int i = 0; i < size; i++)
+int fcs(BYTE * arr,int tam){
+    int sum_bits=0;
+    for (int i = 0; i < tam; i++) // recorrer el arreglo
     {
-        int bit_v;
-        for(int j = 0; j < 8; j++)
+        for (int j = 0; j < 8; j++)
         {
-            bit_v = (arr[i] >> j) & 0x01;
-            suma_bits += bit_v;
+            sum_bits += (arr[i] >> j) & 0x01;
         }
+        
     }
-    return suma_bits;
+    return sum_bits;
 }
 
 bool desempaquetar(Protocolo & proto, int size)
 {
     proto.CMD = proto.Frames[0] & 0x7F;  //7 bits cargados
-    proto.LNG = (proto.Frames[0] >> 7) & 0x01 | (proto.Frames[1] & 0x1F) << 1;  //1 bit restante cargado
+    proto.LNG = (proto.Frames[0] >> 7) & 0x01 | (proto.Frames[1] & 0x1F);  //1 bit restante cargado
+    printf("%d",(proto.Frames[0] >> 7) & 0x01 | (proto.Frames[1] & 0x1F));
 
     if(size != (proto.LNG + 4)) // detección de tamaños incorrectos
     {
+        printf("fue en este coso we %d %d\n",size,proto.LNG+4);
         return false;
     }
     
@@ -55,38 +57,35 @@ bool desempaquetar(Protocolo & proto, int size)
         {                       
             proto.DATA[i] = (proto.Frames[i + 2]);// guarda el byte completo
         }
-
-    unsigned int _fcs = fcs(proto.Frames, proto.LNG); //calcula el fcs de los datos enviados
-
-    proto.FCS[0] = proto.Frames[proto.LNG+2] & 0xFF;  //recupera el fcs recibido en el primer espacio del arreglo
-    proto.FCS[1] = ((proto.Frames[proto.LNG + 3]) << 8) & 0x03;
-    if (_fcs != proto.FCS){
+    }
+    unsigned int _fcs = fcs(proto.Frames, proto.LNG+2); //calcula el fcs de los datos enviados
+    proto.FCS[0] = proto.Frames[proto.LNG+2];  //recupera el fcs recibido en el primer espacio del arreglo
+    proto.FCS[1] = ((proto.Frames[proto.LNG + 3]));
+    if (_fcs != (proto.FCS[0] | proto.FCS[1])){
         return false;
     }
     return true;
+}
+
+void EnviarMensaje(Protocolo & proto){
+    printf("Ingrese su mensaje:\n");
+    scanf("%62s",proto.DATA); //guarda un mensaje ingresado
+    proto.LNG = empaquetar(proto);
+    memcpy(proto.Frames, proto.Frames, proto.LNG + 4);
+}
+
+
+void recibirMensaje(Protocolo &proto)
+{
+    bool estado = desempaquetar(proto, proto.LNG); //ejecuta desempaquetar en el proto dado
+    printf("Se recibio un mensaje con estado %s\n",estado?"Correcto":"Incorrecto"); //imprime por pantalla si el mensaje es correcto
+    printf("El largo del mensaje es %d\n",proto.LNG);
+    printf("El mensaje es %s\n",proto.DATA);
+    system("pause");   
 }
 
 void Cerrar(int comando){
     //limpiar memoria de receptor.cpp
     if (comando=5)
     exit(0);
-}
-
-void EnviarMensaje(Protocolo & proto){
-
-    printf("Ingrese su mensaje\n");
-    scanf("%62s",proto.DATA); //guarda un mensaje ingresado
-
-    proto.LNG = empaquetar(proto);
-
-    memcpy(proto.Frames, proto.Frames, proto.LNG+4);
-}
-
-void recibirMensaje(Protocolo proto)
-{
-    bool estado = desempaquetar(proto, proto.LNG+2); //ejecuta desempaquetar en el proto dado
-
-    printf("Se recibio un mensaje con estado %s\n",estado?"Correcto":"Incorrecto"); //imprime por pantalla si el mensaje es correcto
-    printf("El largo del mensaje es $d\n",proto.LNG);
-    printf("El mensajes es %s\n",proto.DATA);
 }
