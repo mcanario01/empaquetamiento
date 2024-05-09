80c30b1f3f8ba74511c9eef7081302b2fc3568ac
m.canario01 <m.canario01@ufromail.cl>
2024-05-09 01:20:53 -0400

    Adición: Actualización de makefile

--- src/protocolo.h
+++ src/protocolo.h
@@ -3,14 +3,15 @@
 
 #define BYTE unsigned char
 #define LARGO_DATA 63
+#define BYTES_EXTRAS 4
 
 struct Protocolo
 {
-    BYTE CMD;   // 7 bit
-    BYTE LNG;   // 6 bits
+    BYTE CMD;   // 7 bit utiles, 1 bit de relleno
+    BYTE LNG;   // 6 bits utiles, 2 bits de relleno
     BYTE DATA[LARGO_DATA];  // 63 bytes
-    BYTE FCS[2]; // 10 bits
-    BYTE Frames[LARGO_DATA + 4];
+    BYTE FCS[2]; // 10 bits utiles, 6 bits de relleno
+    BYTE Frames[LARGO_DATA + BYTES_EXTRAS];
 };
 
 
