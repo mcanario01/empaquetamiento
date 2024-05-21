9e724d5c4b0637427fcc12b59862740111bd948a
m.canario01 <m.canario01@ufromail.cl>
2024-05-21 14:38:03 -0400

    Refactor delay.cpp to improve PWM configuration and readability

--- src/delay.cpp
+++ src/delay.cpp
@@ -3,11 +3,17 @@
 #include <stdio.h>
 
 #define PWM_PIN 1 // => 18 BCM
-#define PWM_CLOCK_DIVISOR 19.2
-#define PWM_FREQUENCY 500
+#define PWM_RANGE 1024
+#define PWM_FREQUENCY 1
 //Frequency = 19.2 Mhz / clock_divisor / range
 //500 Hz = 19.2 MHz / 19.2 / 20
 //100 Hz = 19.2 MHz / 19.2 / 100
+//9.765625 Hz = 19.2 MHz / 19.2 / 1024
+
+
+
+int obtenerRange(int frequency);
+int obtenerDivisor(int frequency);
 
 int main(){
   if(wiringPiSetup() == -1)
@@ -19,11 +25,11 @@ int main(){
   printf("Configurando PWM...\n");
   pwmSetMode(PWM_MODE_MS); //Mark Space Mode
   printf("Configurando rango y divisor de clock...\n");
-  pwmSetRange(obtenerRange(PWM_FREQUENCY));
-  pwmSetClock(PWM_CLOCK_DIVISOR);
+  pwmSetRange(PWM_RANGE);
+  pwmSetClock(obtenerDivisor(PWM_FREQUENCY));
   
   printf("PWM configurado a %d Hz\n", PWM_FREQUENCY);
-  pwmWrite(PWM_PIN, obtenerRange(500)/2);// 50% del ciclo
+  pwmWrite(PWM_PIN, PWM_RANGE/2);// 50% del ciclo
 
   printf("Ejecutando reloj PWM...\n");
   printf("Presiona Ctrl+C para salir\n");
@@ -34,6 +40,8 @@ int main(){
   return 0;
 }
 
-void obtenerRange(int frequency){
-  return 19200000 / PWM_CLOCK_DIVISOR / frequency;
+
+
+int obtenerDivisor(int frequency){
+  return 19200000 / frequency / PWM_RANGE;
 }
